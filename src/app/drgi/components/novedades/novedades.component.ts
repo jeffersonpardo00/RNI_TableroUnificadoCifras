93b1256bbd5108825a1cc4f4be97f1d81fc05f39
jeffersonpardo00 <63919004+jeffersonpardo00@users.noreply.github.com>
2021-04-01 16:59:25 -0500

    Todo DRGI

--- src/app/drgi/components/novedades/novedades.component.ts
+++ src/app/drgi/components/novedades/novedades.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovedadesComponent implements OnInit {
 
-  title = 'NOVEDAD';
+  title = 'NOVEDADES';
   panelOpenState = false;
 
   constructor() { }
